0aa01ad9248b7ae569027c3ee61021434d6b4688
NavinduNimsara2 <148603247+NavinduNimsara2@users.noreply.github.com>
2025-01-15 15:01:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const ownerNumber = ['263780934873']
 
 //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/session/creds.json')) {
-if(!config.SESSION_ID) return console.log('Please add your session to SESSION_ID env !!')
+if(!config.SESSION_ID) return console.log('otQDxCaJ#YCw6mzUrP6aeHB-TKJ9-DfsUBwJuD8lCYDkrjNWZ68Q')
 const sessdata = config.SESSION_ID
 const filer = File.fromURL(`https://mega.nz/file/${sessdata}`)
 filer.download((err, data) => {
